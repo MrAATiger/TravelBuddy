f53836b497a851f3b1de889c4ae158058e594cbf
sassflo <sass005@cougars.csusm.edu>
2018-05-10 22:41:38 +0200

    Basic Layout finished, Email-Icon changed to basic add-Icon

--- app/src/main/java/travelbuddy/solutions/sass/travelbuddy/MainActivity.java
+++ app/src/main/java/travelbuddy/solutions/sass/travelbuddy/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.add_one_button);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
