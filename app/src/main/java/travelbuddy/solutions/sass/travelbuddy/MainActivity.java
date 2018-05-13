0b7193cd0d151e10e53102a5a927ebefa3c1e658
MrAATiger <alex.angeloussis@web.de>
2018-05-13 10:47:17 +0200

    Test

--- app/src/main/java/travelbuddy/solutions/sass/travelbuddy/MainActivity.java
+++ app/src/main/java/travelbuddy/solutions/sass/travelbuddy/MainActivity.java
@@ -11,7 +11,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+//test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
